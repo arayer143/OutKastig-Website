a4de6b82f4b4027ec921ce557228e078d947027d
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-03-21 14:27:25 -0500

    added to services page

--- assets/js/main.js
+++ assets/js/main.js
@@ -161,4 +161,35 @@ document.addEventListener('DOMContentLoaded', () => {
     aos_init();
   });
 
+
+
+
+
+
+  // TypeScript
+var typing = new Typed(".text", {
+  strings: ["", "Confined Space Rescue", "Hydro Blasting" ,"Vacuum Truck Services", "Hydro Lancing" ,"Tank Cleaning" , "Pipeline Cleaning" ],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true
+});
+
+// GSAP
+gsap.registerPlugin(ScrollTrigger);
+const introsplitTypes = document.querySelectorAll(".left-part h1");
+introsplitTypes.forEach((char, i) => {
+  const i_text = new SplitType(char);
+  gsap.to(i_text.chars, {
+    y: 0,
+    stagger: 0.08, // text splitting transition
+    duration: 0.3 // full text duration
+  });
+});
+
+
+
+
+
+
+
 });
