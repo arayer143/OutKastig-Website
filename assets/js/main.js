502d9e1eb5d18ddb2c88f4a987c76b5e83086df3
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-03-25 15:31:21 -0500

    last removal of unused template items

--- assets/js/main.js
+++ assets/js/main.js
@@ -165,12 +165,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-var options = {
-  rootMargin: '0px',
-  threshold: 1.0
- }
-
-var observer = new IntersectionObserver(callback, options);
 
 
 
