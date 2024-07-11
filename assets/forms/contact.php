2046b8e63801621f386f0bb8dbd32ddaed7b4023
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-07-11 11:49:25 -0500

    small chnage again to the php

--- assets/forms/contact.php
+++ assets/forms/contact.php
@@ -15,6 +15,7 @@
   
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
   $contact->phone = $_POST['phone'];
   $contact->subject = $_POST['subject'];
   $contact->service = $_POST['service'];
