fd8a00e1a1b0c71dfe6421f33404660319e47c4a
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-07-10 23:34:34 -0500

    small change again to php

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -16,8 +16,6 @@ class PHP_Email_Form {
   public $from_name = false;
   public $subject = false;
   public $phone = false;
-  public $email =false;
-  public $service = false;
   public $mailer = false;
   public $smtp = false;
   public $message = '';
