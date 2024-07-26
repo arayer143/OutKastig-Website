906215e1f2d73fd6aef4d7a502172ac7fc64c46d
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-07-26 12:37:51 -0500

    testing of php email form

--- forms/newsletter.php
+++ forms/newsletter.php
@@ -9,7 +9,7 @@
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'alexrayer7@gmail.com';
 
-  if( file_exists($php_email_form = '../assets/libraries/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/libraries/php-email-form/php-email-form-newsletter.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
