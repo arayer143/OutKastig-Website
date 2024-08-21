80bd24161fadb9b7aee2f599d062c7f4c7dd653e
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-08-21 14:58:22 -0500

    small chnage to the php

--- forms/contact.php
+++ forms/contact.php
@@ -19,7 +19,7 @@
   $contact->phone = $_POST['phone'];
   $contact->subject = 'New Message from Outkastindustrial.com';
   $contact->service = $_POST['service'];
-  $mail->addAttachment($_FILES['attachment']);
+  $mail->AddAttachment($attachment['path'], $attachment['name']);
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -37,6 +37,8 @@
   $contact->add_message( $_POST['subject'], 'Company Name');
   $contact->add_message( $_POST['service'], 'Service Requested');
   $contact->add_message( $_POST['message'], 'Message');
+  $contact->add_message( $_POST['message'], 'Message');
+
 
 
   echo $contact->send();
