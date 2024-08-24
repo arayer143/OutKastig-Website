b9acc3ec1d81d505c327d0bac8797f571f2d5d40
arayer143 <89434892+arayer143@users.noreply.github.com>
2024-08-24 12:22:32 -0500

    small change to the careeers.php file

--- forms/careers.php
+++ forms/careers.php
@@ -16,9 +16,8 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->phone = $_POST['phone'];
   $contact->subject = 'New Message from Outkastindustrial.com';
-  $contact->service = $_POST['service'];
+
 
  
 
